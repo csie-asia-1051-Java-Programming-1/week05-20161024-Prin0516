046330dffe45f416ac308ac6a33f3878c142456e
Prin0516 <105021046@live.asia.edu.tw>
2016-10-25 22:17:38 +0800

    ex

--- src/hw/hw02.java
+++ src/hw/hw02.java
@@ -1,10 +1,21 @@
 package hw;
 
+import java.util.Scanner;
+
 public class hw02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scn = new Scanner(System.in);
+		System.out.println("½Ð¿é¤Jn");
+		int n = scn.nextInt();
+		int sum = 0;
+		int i = 1;
+		while (sum < n) {
+			sum = sum + i;
+			i++;
+		}
+		System.out.println(i - 2);
 	}
 
 }
--- src/hw/hw04.java
+++ src/hw/hw04.java
@@ -1,9 +1,20 @@
 package hw;
 
+import java.util.Scanner;
+
 public class hw04 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		System.out.println("½Ð¿é¤J");
+		int n = scn.nextInt();
+		float sum = 0;
+		for (int i = 1; i <= n; i++) {
+			sum = sum + (float)1 / ((2 * i - 1) * (2 * i));
+
+		}
+		System.out.println(sum);
 
 	}
 
